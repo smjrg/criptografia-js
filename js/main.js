68c7780af91b57fb8fc99ffa3f8d182b5211632d
smjrg <juanapjas.jas@gmail.com>
2022-12-24 15:55:43 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,57 @@
+//A letra "e" é convertida para "enter"
+//A letra "i" é convertida para "imes"
+//A letra "a" é convertida para "ai"
+//A letra "o" é convertida para "ober"
+//A letra "u" é convertida para "ufat"
+
+function btnEncriptar(){
+    const inputTexto = document.querySelector(".input");
+    const mensagem = document.querySelector(".mensagem");
+    const textoEncriptado = encriptar(inputTexto.value);
+    mensagem.value = textoEncriptado
+
+    if(btnEncriptar){
+        let div = document.querySelector(".desaparece")
+        let div1 = document.querySelector(".aparece")
+        div.remove();
+        div1.style.display = "block";
+    }
+}
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptada;
+}
+
+function btnDesencriptar(){
+    const inputTexto = document.querySelector(".input");
+    const mensagem = document.querySelector(".mensagem");
+    const textoDesencriptado = desencriptar(inputTexto.value);
+    mensagem.value = textoDesencriptado;
+}
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptada;
+}
+
+function copiar(){
+
+    let output = document.querySelector(".mensagem");
+    navigator.clipboard.writeText(output.value);
+    alert("Texto copiado!");
+}
